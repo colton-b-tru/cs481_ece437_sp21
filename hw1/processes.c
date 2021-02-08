28e74a34fb0d0e844b7c5c7495c4d9a00d1e6f72
Colton Trujillo <colt45x22@gmail.com>
2021-02-08 13:59:23 -0700

    add print statements to see counter values

--- hw1/processes.c
+++ hw1/processes.c
@@ -8,11 +8,15 @@ int counter = 0;
 void child()
 {
     counter += 7;
+    printf("Child counter value:%d\n",counter);
+    printf("Child address:%p\n",(void*)&counter);
 }
 
 void parent()
 {
     counter *= 2;
+    printf("Parent counter value:%d\n",counter);
+    printf("Parent address:%p\n",(void*)&counter);
 }
 
 int main(int argc, char* argv[])
