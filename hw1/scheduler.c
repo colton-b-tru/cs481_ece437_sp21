269d7f11ff4da8404fd13c0966d9b00a84cfb859
Colton Trujillo <colt45x22@gmail.com>
2021-02-10 02:55:14 +0000

    useful comment

--- hw1/scheduler.c
+++ hw1/scheduler.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include "jobs.h"
 #include "scheduler.h"
-
+//Note that in each test, the method is only called once.
+//I don't think you have to worry about the array mutating from
+//the outside.
 // Priority Scheduling -- Jobs with highest priority (lowest number) run first
 //   -- If two jobs with same priority, lower index runs first
 void priority(int n_jobs, Job* jobs)
@@ -22,8 +24,18 @@ void priority(int n_jobs, Job* jobs)
 //    run first
 //    -- If two jobs have same priority, run both in round robin 
 //    -- Time slice for round robin passed as a parameter
+
 void priority_rr(int n_jobs, Job* jobs, int time_slice)
 {
+    qsort(jobs, n_jobs, sizeof(Job), jobcmp);
+    while(n_jobs > 0){
+        jobs->run_job(jobs->time);
+        for(int i = 1; i < n_jobs ;i++){
+            jobs[i-1] = jobs[i]; 
+        }
+        n_jobs--;
+        
+    }
 }
 
 
