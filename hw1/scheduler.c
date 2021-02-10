45346480892e5aaa1b571938251f3b431e23f591
Colton Trujillo <colt45x22@gmail.com>
2021-02-10 02:43:04 +0000

    first crack at q31

--- hw1/scheduler.c
+++ hw1/scheduler.c
@@ -8,6 +8,14 @@
 void priority(int n_jobs, Job* jobs)
 {
     qsort(jobs, n_jobs, sizeof(Job), jobcmp);
+    while(n_jobs > 0){
+        jobs->run_job(jobs->time);
+        for(int i = 1; i < n_jobs ;i++){
+            jobs[i-1] = jobs[i]; 
+        }
+        n_jobs--;
+        
+    }
 }
 
 // Priority Scheduling with Round Robin -- Jobs with highest priority (lowest number)
